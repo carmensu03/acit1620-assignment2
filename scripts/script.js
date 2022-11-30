267dd5a0de8424be5dab805d591082547cca8820
Carmen Su <csu30@my.bcit.ca>
2022-11-29 19:22:22 -0800

    added changeBG function into js

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,22 @@
 const clickMeButton = document.querySelector(".dark");
+const body = document.querySelector("body");
 
 function changeText()
 {
     if (clickMeButton.textContent === "Dark Theme")
     {
         clickMeButton.textContent = "Light Theme";
+        changeBG()
     }
-    else 
+    else
     {
         clickMeButton.textContent = "Dark Theme"
+        changeBG()
     }
 }
+
+function changeBG() {
+    body.classList.toggle("BGcolor");
+}
+
 clickMeButton.addEventListener("click", changeText)
