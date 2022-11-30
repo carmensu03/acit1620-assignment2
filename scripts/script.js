b31ef321b2498e4bbbd8e86b8929e6fac655a7ca
Carmen Su <csu30@my.bcit.ca>
2022-11-29 19:49:45 -0800

    changed side notes background color

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const clickMeButton = document.querySelector(".dark");
 const body = document.querySelector("body");
+const sideNotes = document.querySelector("aside");
 
 function changeText()
 {
@@ -7,17 +8,24 @@ function changeText()
     {
         clickMeButton.textContent = "Light Theme";
         changeBG()
+        changeSideNotes()
     }
     else
     {
-        clickMeButton.textContent = "Light Theme"
+        clickMeButton.textContent = "Dark Theme"
         changeBG()
+        changeSideNotes()
     }
 }
 
 function changeBG() 
 {
     body.classList.toggle("BGcolor");
+}
+
+function changeSideNotes() 
+{
+    sideNotes.classList.toggle("SideNotesColor");
 }
 
 clickMeButton.addEventListener("click", changeText)
