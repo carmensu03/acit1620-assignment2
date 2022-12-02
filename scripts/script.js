40d1e30c2a911732e7e36ccb4a5a31aab7735d07
Carmen Su <csu30@my.bcit.ca>
2022-12-01 16:44:13 -0800

    made both new note and save button to same color

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const clickMeButton = document.querySelector(".dark");
 const body = document.querySelector("body");
 const sideNotes = document.querySelector("aside");
 const textArea = document.querySelector("textarea");
-const blueButton = document.querySelector(".blue");
+const blueButton = document.querySelectorAll(".blue");
 const imgLogo = document.querySelectorAll("img");
 const lightButton = document.querySelector(".dark");
 const redButton = document.querySelector(".red");
@@ -27,9 +27,11 @@ function changeTheme()
     body.classList.toggle("DarkBGcolor");
     sideNotes.classList.toggle("DarkSideNotesColor");
     textArea.classList.toggle("DarkTextArea");
-    blueButton.classList.toggle("DarkBlueButton");
     lightButton.classList.toggle("DarkLightButton");
     redButton.classList.toggle("DarkRedButton");
+    for (let blue of blueButton) {
+        blue.classList.toggle("DarkBlueButton");
+    };
     for (let image of imgLogo) {
         image.classList.toggle("DarkLogoColor");
     };
